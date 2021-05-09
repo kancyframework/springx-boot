ceddc0a94677fc0e56a6b492c984beed78276265
kancy666 <kancy793272861>
2021-05-09 18:53:51 +0800

    注册表

--- demo/demo-spring-boot-starter-gui/src/main/java/com/github/kancyframework/springx/demo/Application.java
+++ demo/demo-spring-boot-starter-gui/src/main/java/com/github/kancyframework/springx/demo/Application.java
@@ -4,11 +4,15 @@ import com.github.kancyframework.springx.boot.SpringApplication;
 import com.github.kancyframework.springx.boot.SpringBootApplication;
 import com.github.kancyframework.springx.demo.ui.DemoFrame;
 import com.github.kancyframework.springx.swing.SwingSpringApplication;
+import com.github.kancyframework.springx.utils.RandomUtils;
+import com.github.kancyframework.springx.utils.StringRedisUtils;
 
 @SpringBootApplication
 public class Application implements SwingSpringApplication<DemoFrame> {
 
     public static void main(String[] args) {
+        StringRedisUtils.set("random", RandomUtils.nextLong());
+        StringRedisUtils.set("time", System.currentTimeMillis());
         SpringApplication.run(Application.class, args);
     }
 }
