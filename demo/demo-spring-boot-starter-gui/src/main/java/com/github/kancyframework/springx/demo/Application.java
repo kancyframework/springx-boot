4b883f1d963662d9e6fb959776439e1e1db395be
kancy <793272861@qq.com>
2021-08-07 17:39:50 +0800

    0.0.4 RELEASE

--- demo/demo-spring-boot-starter-gui/src/main/java/com/github/kancyframework/springx/demo/Application.java
+++ demo/demo-spring-boot-starter-gui/src/main/java/com/github/kancyframework/springx/demo/Application.java
@@ -6,13 +6,14 @@ import com.github.kancyframework.springx.demo.ui.DemoFrame;
 import com.github.kancyframework.springx.swing.SwingSpringApplication;
 import com.github.kancyframework.springx.utils.RandomUtils;
 import com.github.kancyframework.springx.utils.RegistryRedisUtils;
+import com.github.kancyframework.springx.utils.RegistryUtils;
 
 @SpringBootApplication
 public class Application implements SwingSpringApplication<DemoFrame> {
 
     public static void main(String[] args) {
-        RegistryRedisUtils.set("random", RandomUtils.nextLong());
-        RegistryRedisUtils.set("time", System.currentTimeMillis());
+        RegistryRedisUtils.set("a",1);
+        RegistryUtils.setValue("b", "1");
         SpringApplication.run(Application.class, args);
     }
 }
