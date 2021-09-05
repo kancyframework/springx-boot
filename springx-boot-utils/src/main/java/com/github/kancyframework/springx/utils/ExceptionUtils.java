b971c65c3f0cbb1070ef3fe0b2977e9d4c61c3aa
kancy <793272861@qq.com>
2021-09-06 01:25:13 +0800

    异常

--- springx-boot-utils/src/main/java/com/github/kancyframework/springx/utils/ExceptionUtils.java
+++ springx-boot-utils/src/main/java/com/github/kancyframework/springx/utils/ExceptionUtils.java
@@ -2,6 +2,7 @@ package com.github.kancyframework.springx.utils;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * ExceptionUtils
@@ -10,6 +11,29 @@ import java.io.StringWriter;
  * @date 2021/8/27 13:03
  */
 public class ExceptionUtils {
+    /**
+     * 将CheckedException转换为UncheckedException.
+     *
+     * @param e Throwable
+     * @return {RuntimeException}
+     */
+    public static RuntimeException unchecked(Throwable e) {
+        if (e instanceof Error) {
+            throw (Error) e;
+        } else if (e instanceof IllegalAccessException ||
+                e instanceof IllegalArgumentException ||
+                e instanceof NoSuchMethodException) {
+            return new IllegalArgumentException(e);
+        } else if (e instanceof InvocationTargetException) {
+            return ExceptionUtils.runtime(((InvocationTargetException) e).getTargetException());
+        } else if (e instanceof RuntimeException) {
+            return (RuntimeException) e;
+        } else if (e instanceof InterruptedException) {
+            Thread.currentThread().interrupt();
+        }
+        return ExceptionUtils.runtime(e);
+    }
+
     /**
      * 不采用 RuntimeException 包装，直接抛出，使异常更加精准
      *
